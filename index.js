4c643e212b59d709045f3b0d1e388cfbe2340fdb
sudhanshubsr <sudhanshubsr.dev@gmail.com>
2024-09-12 00:33:42 +0000

    chore : Refactor code to add Swagger documentation for API endpoints

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import fs from 'fs'
 import compression from "compression";
-import docRouter from './src/routes/documentation.routes.js';
 import chatRouter from './src/routes/randomresponse.routes.js';
+import swaggerUi from 'swagger-ui-express'
 dotenv.config();
 
 
 
 const app = express();
+const apiDocs = JSON.parse(fs.readFileSync('./swagger.json', 'utf-8'))
 
 app.use(helmet());  // Using Helmet to protect against some common attacks
 app.use(compression()); // Using compression to size of the data sent from server to the client
 
 // Routes
 app.use('/api/chatbot', chatRouter)
-app.use('/api/docs',docRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
 
 // Centralized Error Handling
--- src/routes/documentation.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-router.get('/docs', (req,res)=>{
-    res.status(201).json({message: "API Docs"})
-})
-
-export default router
